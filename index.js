a5a76dfa765ae22c59e9620c5413b514c214e433
xelagfx1 <alxsgraphics211@gmail.com>
2020-11-24 14:18:11 -0600

    add passport js

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+passport.use(new GoogleStrategy());
+// 
 
 app.get('/', function (req, res) {
     res.send({Hi: 'there.'})
