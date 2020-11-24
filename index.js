a6fd2a5cec55950dd7541fcd95a1ee88a2e09606
xelagfx1 <alxsgraphics211@gmail.com>
2020-11-24 13:18:28 -0600

    update port

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.get('/', function (req, res) {
     res.send({Hi: 'there.'})
@@ -10,4 +11,6 @@ app.get('/user/:id', function (req, res) {
     res.send('user ' + req.params.id)
 })
 
-app.listen(5000);
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
