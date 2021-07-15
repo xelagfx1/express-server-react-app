e599d5d539bfec53a61683412d6d83e5b9a32220
xelagfx1 <alxsgraphics211@gmail.com>
2021-07-15 17:07:31 -0500

    config env variables

--- services/passport.js
+++ services/passport.js
@@ -16,8 +16,8 @@ passport.deserializeUser((id, done) => {
 });
 
 passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    clientID: s_keys.googleClientID || process.env.GOOGLE_CLIENT_ID,
+    clientSecret:  s_keys.googleClientSecret || process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: '/auth/google/callback'
   },
   function(accessToken, refreshToken, profile, done) {
