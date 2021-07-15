a114ccc4e97e2ab21e4735ce4fcd5a894abaff5a
xelagfx1 <alxsgraphics211@gmail.com>
2021-07-15 14:48:27 -0500

    auth updates - google strategy

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,19 +1,26 @@
 const authPassport = require('passport');
 module.exports = (app) => {
-    app.get('/', function (req, res) {
-        res.send({Hi: 'there.'})
-    })
-    
-    app.get('/auth/google',
-    authPassport.authenticate('google', { scope: ['profile', 'email'] }));
-    
-    app.get('/auth/google/callback', 
-    authPassport.authenticate('google', { 
-        failureRedirect: '/'
-      }),
-      function(req, res) {
-        // Successful authentication, redirect home.
-        //res.redirect('/');
-        console.log(res)
-      });
+  app.get('/', function (req, res) {
+      res.send({Hi: 'there.'})
+  })
+  
+  app.get('/auth/google',
+  authPassport.authenticate('google', { scope: ['profile', 'email'] }));
+  
+  app.get('/auth/google/callback', 
+  authPassport.authenticate('google', { 
+    failureRedirect: '/'
+  }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+    //console.log(res)
+  });
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.send("You have been logged out. Goodbye.");
+  })
+  app.get('/api/user', (req, res) => {
+    res.send(req.user);
+  })
 };
--- services/passport.js
+++ services/passport.js
@@ -1,19 +1,43 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('../config/keys');
+const s_keys = require('../config/keys');
+const mongoose_p = require('mongoose');
+
+const User = mongoose_p.model('users');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
 
 passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: '/auth/google/callback'
   },
-  function(accessToken, refreshToken, profile, cb) {
-    // User.findOrCreate({ googleId: profile.id }, function (err, user) {
-    //   return cb(err, user);
-    // });
-    console.log('profile:' + profile);
-    console.log('accessToken:' + accessToken);
-    console.log('cb:' + cb);
+  function(accessToken, refreshToken, profile, done) {
+    User.findOne({ googleId: profile.id }).then((existingUser) => {
+      if(existingUser) {
+        done(null, existingUser);
+      } else {
+        new User({ 
+          googleId: profile.id,
+          name: profile.fullName,
+          email: profile.email
+        })
+        .save()
+        .then(user => done(null, user));
+      }
+    })
+
+    // console.log('profile:' + JSON.stringify(profile));
+    // console.log('accessToken:' + accessToken);
+    // console.log('cb:' + cb);
 
   }
 ));
